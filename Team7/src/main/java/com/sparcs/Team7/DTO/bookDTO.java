86543d795e3eb6cc265e87db0921c78fe8b90ca5
anismynm <ddoe0318@naver.com>
2024-07-31 20:56:11 +0900

    update)

--- Team7/src/main/java/com/sparcs/Team7/DTO/bookDTO.java
+++ Team7/src/main/java/com/sparcs/Team7/DTO/bookDTO.java
@@ -1,10 +1,12 @@
 package com.sparcs.Team7.DTO;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class bookDTO {
     private String bookTitle;
     private String color;
--- Team7/src/main/java/com/sparcs/Team7/DTO/rpsaveDTO.java
+++ Team7/src/main/java/com/sparcs/Team7/DTO/rpsaveDTO.java
@@ -1,10 +1,12 @@
 package com.sparcs.Team7.DTO;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class rpsaveDTO {
     private String rpId;
     private String rpTitle;
